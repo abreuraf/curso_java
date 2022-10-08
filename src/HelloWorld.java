244f85ef5ae4e685cc44a70319f7e6510ea3f720
Rafael Abreu <abreurafaelmaia@gmail.com>
2022-10-08 15:10:23 -0300

    Variáveis

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome back to Java World!");
+		
+	}
+
+}
---
+++ src/Variaveis.java
@@ -0,0 +1,7 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int age = 33;
+        String name = "Rafael Abreu";
+        System.out.println("Nome: "+name+" - Idade: "+age);
+    }
+}
